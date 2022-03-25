9381a46d3d0cfdd770d80c092c65cf54f0bc4d3e
Ishuah Kariuki <kariuki@ishuah.com>
2022-03-25 20:48:42 +0300

    Add data_key field to layer model

--- core/models.py
+++ core/models.py
@@ -12,6 +12,7 @@ class Map(models.Model):
 
 class Layer(models.Model):
     name = models.CharField(max_length=50)
+    data_key = models.CharField(max_length=20)
     my_map = models.ForeignKey(Map, on_delete=models.CASCADE)
     
     def __str__(self):
